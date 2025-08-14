2176bcf54492687eab6a85fed73d6a5805023f17
avnikittsev <nikittsev.anton@gmail.com>
2025-08-14 22:33:48 +0300

    Add not empty modifier

--- application/models/interests/delete/delete_interest_request_dto.go
+++ application/models/interests/delete/delete_interest_request_dto.go
@@ -3,5 +3,5 @@ package delete
 import "chat_app_backend/internal/extensions"
 
 type DeleteInterestRequestDto struct {
-	ID extensions.UUID `uri:"id"`
+	ID extensions.UUID `uri:"id" validator:"not_empty"`
 }
